2fd8406afd1fa85456ad6dd8f9736bfebdc0fb75
Diocleniax789 <julioramdom123@gmail.com>
2025-04-30 22:30:51 -0300

    se agregaron comentarios en el codigo

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const botonAtras = document.getElementById('button-back');
 const botonAdelante = document.getElementById('button-next');
+const apiKey = '';
+
+// se agregaron las funcionalidades
 
 botonAtras.addEventListener('click', () => console.log(botonAtras));
 botonAdelante.addEventListener('click', () => console.log(botonAdelante));
