35b108490dd9643e78c9aae410ee7c8942e4011d
Diocleniax789 <julioramdom123@gmail.com>
2025-05-01 21:13:27 -0300

    se ha implementado correctamente los botones

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const botonAtras = document.getElementById('button-back');
 const botonAdelante = document.getElementById('button-next');
-const apiKey = '';
+let nroPagina = 1;
 
-// se agregaron las funcionalidades
+//const cargarPeliculas = require('./cargarPeliculas.js');
 
-botonAtras.addEventListener('click', () => console.log(botonAtras));
-botonAdelante.addEventListener('click', () => console.log(botonAdelante));
+const paginaAnterior = () => {
+	if (nroPagina < 1) {
+		botonAtras.style.pointerEvents = "none";
+		botonAtras.style.cursor = "none";
+		nroPagina = 1;
+		console.log('hasta aca llegaste');
+	} else {
+		nroPagina--;
+		botonAdelante.style.cursor = "pointer";
+		botonAdelante.style.pointerEvents = "auto";
+		console.log(`nro de pagina, ${nroPagina}`);
+	}
+}
+
+const paginaSiguiente = () => {
+	if (nroPagina < 8) {
+		nroPagina++;
+		botonAtras.style.cursor = "pointer";
+		botonAtras.style.pointerEvents = "auto";
+		console.log(`nro de pagina, ${nroPagina}`);
+	} else {
+		botonAdelante.style.cursor = "none";
+		botonAdelante.style.pointerEvents = "none";
+		console.log('hasta aca llegaste');
+	}
+}
+
+
+botonAtras.addEventListener('click', paginaAnterior);
+botonAdelante.addEventListener('click', paginaSiguiente);
