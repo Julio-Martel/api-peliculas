8d22f8b16483e59866cc7c553eb7157f59e178d8
Diocleniax789 <julioramdom123@gmail.com>
2025-05-05 21:32:19 -0300

    se han implementado comentarios como ayuda

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
 const botonAtras = document.getElementById('button-back');
 const botonAdelante = document.getElementById('button-next');
+const contenedorPosterPeliculas = document.getElementById('movies');
 let nroPagina = 1;
 
-//const cargarPeliculas = require('./cargarPeliculas.js');
+import { cargarPeliculas } from 'cargarPeliculas.js';
+
+
+// funcion que permite volver a la pagina anterior
 
 const paginaAnterior = () => {
 	if (nroPagina < 1) {
 		botonAtras.style.pointerEvents = "none";
 		botonAtras.style.cursor = "none";
 		nroPagina = 1;
-		console.log('hasta aca llegaste');
 	} else {
 		nroPagina--;
 		botonAdelante.style.cursor = "pointer";
 		botonAdelante.style.pointerEvents = "auto";
-		console.log(`nro de pagina, ${nroPagina}`);
 	}
+	cargarPeliculas(nroPagina, contenedorPosterPeliculas);
 }
+
+// funcion que permite ir a la pagina siguiente
 
 const paginaSiguiente = () => {
 	if (nroPagina < 8) {
 		nroPagina++;
 		botonAtras.style.cursor = "pointer";
 		botonAtras.style.pointerEvents = "auto";
-		console.log(`nro de pagina, ${nroPagina}`);
 	} else {
 		botonAdelante.style.cursor = "none";
 		botonAdelante.style.pointerEvents = "none";
-		console.log('hasta aca llegaste');
 	}
+	cargarPeliculas(nroPagina, contenedorPosterPeliculas);
 }
-
 
 botonAtras.addEventListener('click', paginaAnterior);
 botonAdelante.addEventListener('click', paginaSiguiente);
+
+cargarPeliculas(nroPagina, contenedorPosterPeliculas);
